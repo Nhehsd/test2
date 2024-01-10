869c340386ca8d98eff34f280fd36e1bf1211412
Nhehsd <156100987+Nhehsd@users.noreply.github.com>
2024-01-10 11:13:49 +0000

    Update app.js
    Signed-off-by: Nhehsd <156100987+Nhehsd@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ document.addEventListener("DOMContentLoaded", function() {
   const statusContainer = document.getElementById("status-container");
 
 
-  const apiUrl = `https://api.tfl.gov.uk/line/mode/tube/status`;
+  const tubeApiUrl = `https://api.tfl.gov.uk/line/mode/tube/status`;
+  const elizabethLineApiUrl = 'https://api.tfl.gov.uk/line/elizabeth/status';
 
-  // Fetch tube status data from TfL API
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => displayTubeStatus(data))
-    .catch(error => console.error("Error fetching data:", error));
+ // Fetch tube status data from TfL API
+  const fetchTubeData = fetch(tubeApiUrl).then((response) => response.json());
+  const fetchElizabethLineData = fetch(elizabethLineApiUrl).then((response) => response.json());
+
+  // Use Promise.all to wait for both API requests to complete
+  Promise.all([fetchTubeData, fetchElizabethLineData])
+    .then(([tubeData, elizabethLineData]) => displayTubeStatus(tubeData, elizabethLineData))
+    .catch((error) => console.error("Error fetching data:", error));
 
   // Function to display tube status
-  function displayTubeStatus(data) {
+  function displayTubeStatus(tubeData, elizabethLineData) {
     const lineColors = {
       Bakerloo: "#996633",
       Central: "#CC3333",
-      Circle: "#FF8800",
+      Circle: "#E1A700",
       District: "#006633",
       Hammersmith: "#CC9999",
       Jubilee: "#868F98",
@@ -24,18 +28,24 @@ document.addEventListener("DOMContentLoaded", function() {
       Piccadilly: "#0019a8",
       Victoria: "#0099CC",
       Waterloo: "#66CCCC",
-      Elizabeth: "#9E579D"
+      Elizabeth: "#9E579D" // Elizabeth line color
       // Add more lines and colors as needed
     };
 
-  // Function to display tube status
-  const tubeLines = data.map((line) => {
+    const tubeLines = tubeData.map((line) => {
       const lineColor = lineColors[line.name] || "#000000"; // Default to black if color is not specified
       return `<div class="line" style="color: ${lineColor};">
-                <strong>${line.name}</strong>: <span class="status">${line.lineStatuses[0].statusSeverityDescription}</span>
+                <strong>${line.name}</strong>: ${line.lineStatuses[0].statusSeverityDescription}
               </div>`;
     });
 
-    statusContainer.innerHTML = tubeLines.join("");
+    const elizabethLine = elizabethLineData.map((line) => {
+      const lineColor = lineColors[line.name] || "#000000"; // Default to black if color is not specified
+      return `<div class="line" style="color: ${lineColor};">
+                <strong>${line.name}</strong>: ${line.lineStatuses[0].statusSeverityDescription}
+              </div>`;
+    });
+
+    statusContainer.innerHTML = tubeLines.concat(elizabethLine).join("");
   }
 });
